80e613be378f3bcaf167a31a09612e3918462f66
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2022-01-31 23:47:05 -0300

    Terceira Concuida

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,8 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
-function GlobalStyle(){
-  return(
-      <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-      `}</style>
-  );
-}
+
 function Title(props){
   console.log(props.children);
   const Tag = props.tag || 'h1';
@@ -36,7 +11,7 @@ function Title(props){
       <Tag>{props.children}</Tag>
       <style jsx>{`
       ${Tag} {
-          color: ${appConfig.theme.colors.neutrals['900']};
+          color: ${appConfig.theme.colors.neutrals['050']};
           font-size: 24px;
           font-weight: 600;
       }
@@ -63,11 +38,12 @@ function Title(props){
   // export default HomePage
 
   export default function PaginaInicial() {
-    const username = 'MikhaelPedro';
-  
+    //const username = 'mikhaelpedro';
+    const [username, setUserName] = React.useState('MikhaelPedro');
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -94,6 +70,12 @@ function Title(props){
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                console.log('Alguém submeteu o form');
+                roteamento.push('/chat');        
+                //window.location.href = '/chat';
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -103,8 +85,29 @@ function Title(props){
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+              
+              {/* <input
+                type="text"
+                value= {username}
+                onChange={function (event) {
+                    console.log('usuario digitou', event.target.value)
+                    // Onde ta o valor?
+                    const valor = event.target.value;
+                    // Trocar o valor da variavel
+                    // Através do react e avise quem precisa
+                    // setUserName(valor);
+                }}
+              /> */}
               <TextField
+              value={username}
+              onChange={function (event) {
+                console.log('usuario digitou', event.target.value)
+                // Onde ta o valor?
+                const valor = event.target.value;
+                // Trocar o valor da variavel
+                // Através do react e avise quem precisa
+                setUserName(valor);
+              }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
