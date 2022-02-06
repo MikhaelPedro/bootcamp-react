ec700d1f26e67a2333d7e5cb739b0f72f221bdd9
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2022-02-06 18:01:53 -0300

    Aula 5 Concluída

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ function Title(props){
               onSubmit={function (infosDoEvento) {
                 infosDoEvento.preventDefault();
                 console.log('Alguém submeteu o form');
-                roteamento.push('/chat');        
+                roteamento.push(`/chat?username=${username}`);        
                 //window.location.href = '/chat';
               }}
               styleSheet={{
